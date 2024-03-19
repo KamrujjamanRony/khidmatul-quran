c42bd7bbdcae236eb521cfcd25c3b2ee51460a2f
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-03-20 03:17:17 +0600

    pdf implement 2

--- src/app/pages/zakat-calculator/zakat-calculator.component.ts
+++ src/app/pages/zakat-calculator/zakat-calculator.component.ts
@@ -69,7 +69,7 @@ export class ZakatCalculatorComponent {
     html2canvas(data, {scale:2}).then(canvas => {
       let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
       pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
-      pdf.setFontSize(12);
+      pdf.setFontSize(20);
       pdf.save('MYPdf.pdf'); // Generated PDF   
     }); 
   }
