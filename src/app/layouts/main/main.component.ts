3708f9c910d0b29f93b80819049fcb0e14c37350
KamrujjamanRony <kamrujjaman0228@gmail.com>
2025-01-22 10:47:37 +0600

    add footer

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
     selector: 'app-main',
     standalone: true,
     templateUrl: './main.component.html',
     styleUrl: './main.component.css',
-    imports: [CommonModule, RouterOutlet, NavbarComponent, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, HeaderComponent, FooterComponent]
 })
 export class MainComponent {
 }
